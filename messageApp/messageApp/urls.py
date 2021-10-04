13ff3c80e4d03a1e2161fbad9a2261b736d23b95
Katrina (Yuqiao Hu) <53132886+Katrina0406@users.noreply.github.com>
2021-10-03 21:20:59 -0400

    Add files via upload

---
+++ messageApp/messageApp/urls.py
@@ -0,0 +1,28 @@
+"""messageApp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from connect import wechat
+from connect import views
+from connect import dingding
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('user/',views.logindb,name='logindb'),
+    path('reg/',wechat.runMsg,name='runMsg'),
+    path('ding/',dingding.sendDD,name='sendDD'),
+]
