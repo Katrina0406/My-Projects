54e8e9a67f6c9497a85dbe670e30a2db5c376bf7
Katrina (Yuqiao Hu) <53132886+Katrina0406@users.noreply.github.com>
2021-10-06 00:22:13 -0400

    Update and rename deepLearning/LSTM_model.py to deepLearning/LSTM_Model/LSTM_model.py

--- deepLearning/LSTM_Model/LSTM_model.py
+++ deepLearning/LSTM_Model/LSTM_model.py
@@ -24,63 +24,63 @@ def train(config, logger, train_and_valid_data):
         vis = visdom.Visdom(env='model_pytorch')
 
     train_X, train_Y, valid_X, valid_Y = train_and_valid_data
-    train_X, train_Y = torch.from_numpy(train_X).float(), torch.from_numpy(train_Y).float()     # 先转为Tensor
-    train_loader = DataLoader(TensorDataset(train_X, train_Y), batch_size=config.batch_size)    # DataLoader可自动生成可训练的batch数据
-
+    train_X, train_Y = torch.from_numpy(train_X).float(), torch.from_numpy(train_Y).float()     # change to Tensor
+    train_loader = DataLoader(TensorDataset(train_X, train_Y), batch_size=config.batch_size)    # DataLoader automatically generates trainable batch data
     valid_X, valid_Y = torch.from_numpy(valid_X).float(), torch.from_numpy(valid_Y).float()
     valid_loader = DataLoader(TensorDataset(valid_X, valid_Y), batch_size=config.batch_size)
 
-    device = torch.device("cuda:0" if config.use_cuda and torch.cuda.is_available() else "cpu") # CPU训练还是GPU
-    model = Net(config).to(device)      # 如果是GPU训练， .to(device) 会把模型/数据复制到GPU显存中
-    if config.add_train:                # 如果是增量训练，会先加载原模型参数
+    device = torch.device("cuda:0" if config.use_cuda and torch.cuda.is_available() else "cpu") # CPU or GPU to train
+    model = Net(config).to(device)      # if train on GPU， .to(device) will copy model/data to GPU
+    if config.add_train:                # if add_train，will load previous model's params first
         model.load_state_dict(torch.load(config.model_save_path + config.model_name))
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
-    criterion = torch.nn.MSELoss()      # 这两句是定义优化器和loss
+    criterion = torch.nn.MSELoss()      # define optimizer and loss
 
     valid_loss_min = float("inf")
     bad_epoch = 0
     global_step = 0
     for epoch in range(config.epoch):
         logger.info("Epoch {}/{}".format(epoch, config.epoch))
-        model.train()                   # pytorch中，训练时要转换成训练模式
+        model.train()                   # in pytorch，turn to train mode
         train_loss_array = []
         hidden_train = None
         for i, _data in enumerate(train_loader):
             _train_X, _train_Y = _data[0].to(device),_data[1].to(device)
-            optimizer.zero_grad()               # 训练前要将梯度信息置 0
-            pred_Y, hidden_train = model(_train_X, hidden_train)    # 这里走的就是前向计算forward函数
+            optimizer.zero_grad()               # gradient = 0
+            pred_Y, hidden_train = model(_train_X, hidden_train)    # forward function
 
             if not config.do_continue_train:
-                hidden_train = None             # 如果非连续训练，把hidden重置即可
+                hidden_train = None             # non-continuous training，reset hidden value
             else:
                 h_0, c_0 = hidden_train
-                h_0.detach_(), c_0.detach_()    # 去掉梯度信息
+                h_0.detach_(), c_0.detach_()    # eliminate gradient info
                 hidden_train = (h_0, c_0)
-            loss = criterion(pred_Y, _train_Y)  # 计算loss
-            loss.backward()                     # 将loss反向传播
-            optimizer.step()                    # 用优化器更新参数
+            loss = criterion(pred_Y, _train_Y)  # calculate loss
+            loss.backward()                     # spread loss backwards
+            optimizer.step()                    # update params with optimizer
             train_loss_array.append(loss.item())
             global_step += 1
-            if config.do_train_visualized and global_step % 100 == 0:   # 每一百步显示一次
+            if config.do_train_visualized and global_step % 100 == 0:   # show every 100 steps
                 vis.line(X=np.array([global_step]), Y=np.array([loss.item()]), win='Train_Loss',
                          update='append' if global_step > 0 else None, name='Train', opts=dict(showlegend=True))
 
-        # 以下为早停机制，当模型训练连续config.patience个epoch都没有使验证集预测效果提升时，就停止，防止过拟合
-        model.eval()                    # pytorch中，预测时要转换成预测模式
+        # The early stopping mechanism - when the model training for consecutive config.patience epochs does not 
+        # improve the prediction effect of the validation set, it stops to prevent over-fitting
+        model.eval()                    # in pytorch，turn to predict mode
         valid_loss_array = []
         hidden_valid = None
         for _valid_X, _valid_Y in valid_loader:
             _valid_X, _valid_Y = _valid_X.to(device), _valid_Y.to(device)
             pred_Y, hidden_valid = model(_valid_X, hidden_valid)
             if not config.do_continue_train: hidden_valid = None
-            loss = criterion(pred_Y, _valid_Y)  # 验证过程只有前向计算，无反向传播过程
+            loss = criterion(pred_Y, _valid_Y)  
             valid_loss_array.append(loss.item())
 
         train_loss_cur = np.mean(train_loss_array)
         valid_loss_cur = np.mean(valid_loss_array)
         logger.info("The train loss is {:.6f}. ".format(train_loss_cur) +
               "The valid loss is {:.6f}.".format(valid_loss_cur))
-        if config.do_train_visualized:      # 第一个train_loss_cur太大，导致没有显示在visdom中
+        if config.do_train_visualized:      # the first train_loss_cur is so large that it doens't display in visdom
             vis.line(X=np.array([epoch]), Y=np.array([train_loss_cur]), win='Epoch_Loss',
                      update='append' if epoch > 0 else None, name='Train', opts=dict(showlegend=True))
             vis.line(X=np.array([epoch]), Y=np.array([valid_loss_cur]), win='Epoch_Loss',
@@ -89,36 +89,36 @@ def train(config, logger, train_and_valid_data):
         if valid_loss_cur < valid_loss_min:
             valid_loss_min = valid_loss_cur
             bad_epoch = 0
-            torch.save(model.state_dict(), config.model_save_path + config.model_name)  # 模型保存
+            torch.save(model.state_dict(), config.model_save_path + config.model_name)  # save model
         else:
             bad_epoch += 1
-            if bad_epoch >= config.patience:    # 如果验证集指标连续patience个epoch没有提升，就停掉训练
+            if bad_epoch >= config.patience:    # If the validation set index does not improve for consecutive patience epochs, stop training
                 logger.info(" The training stops early in epoch {}".format(epoch))
                 break
 
 
 def predict(config, test_X):
-    # 获取测试数据
+    # get prediction data
     test_X = torch.from_numpy(test_X).float()
     test_set = TensorDataset(test_X)
     test_loader = DataLoader(test_set, batch_size=1)
 
-    # 加载模型
+    # load model
     device = torch.device("cuda:0" if config.use_cuda and torch.cuda.is_available() else "cpu")
     model = Net(config).to(device)
-    model.load_state_dict(torch.load(config.model_save_path + config.model_name))   # 加载模型参数
+    model.load_state_dict(torch.load(config.model_save_path + config.model_name))   # load model params
 
-    # 先定义一个tensor保存预测结果
+    # define a tensor to save predicting results
     result = torch.Tensor().to(device)
 
-    # 预测过程
+    # prediction
     model.eval()
     hidden_predict = None
     for _data in test_loader:
         data_X = _data[0].to(device)
         pred_X, hidden_predict = model(data_X, hidden_predict)
-        # if not config.do_continue_train: hidden_predict = None    # 实验发现无论是否是连续训练模式，把上一个time_step的hidden传入下一个效果都更好
+        # if not config.do_continue_train: hidden_predict = None  # regardless of whether continuous training mode, it;s better to pass hidden of previous time_step to next one
         cur_pred = torch.squeeze(pred_X, dim=0)
         result = torch.cat((result, cur_pred), dim=0)
 
-    return result.detach().cpu().numpy()    # 先去梯度信息，如果在gpu要转到cpu，最后要返回numpy数据
+    return result.detach().cpu().numpy()    # elininare gradient info first，return numpy data in the end
